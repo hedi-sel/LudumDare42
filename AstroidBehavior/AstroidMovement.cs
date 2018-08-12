9081a5f0863e1a85852e0549327cd33ba5c7f5f4
unknown <abdelwaheb.moalla@gmail.com>
2018-08-12 20:21:56 +0100

    adding astoid movement

---
+++ AstroidBehavior/AstroidMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidMovement : MonoBehaviour {
+	public Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// transform.position = Vector2.MoveTowards (transform.position, Vector2 (transform.position.x, GameHandler.instance.character.position.y, Time.deltaTime * 3));
+	}
+}
